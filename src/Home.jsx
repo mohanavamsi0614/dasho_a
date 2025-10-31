66ad8184cd32d9c4420b7bfb9bbf32c99e9be31f
mohanavamsi0614 <mohanavamsi14@gmail.com>
2025-10-31 22:05:57 +0530

    done

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,4 @@
 import { useNavigate } from "react-router";
-import img from "./assets/image1.png";
-import img1 from "./assets/image.png";
 import { BackgroundBeams } from "./components/ui/background-beams";
 
 function Home() {
@@ -9,17 +7,15 @@ function Home() {
     <div className="bg-[#212121] font-poppins flex w-full gap-10  h-screen justify-center items-center">
       <BackgroundBeams className="fixed inset-0 z-0" />
       <div
-        className="relative z-10 cursor-pointer bg-[#181818] hover:bg-gray-300 p-4 rounded-xl text-white text-center font-semibold"
+        className="relative z-10 cursor-pointer  border-2  w-80 p-15 flex justify-center items-center hover:bg-gray-300 hover:text-black hover:border-black rounded-xl text-white text-center font-semibold"
         onClick={() => nav("/qr")}
       >
-        <img src={img} className=" w-52 h-52" />
-        QR Code Event
+        <p className=" w-fu">QR Code Event</p>
       </div>
       <div
-        className="relative z-10 cursor-pointer bg-[#181818] hover:bg-gray-300 p-4 rounded-xl text-white text-center font-semibold"
+        className="relative z-10 cursor-pointer  border-2  w-80 p-15 flex justify-center items-center hover:bg-gray-300 hover:text-black hover:border-black rounded-xl text-white text-center font-semibold"
         onClick={() => nav("/hackthon")}
       >
-        <img src={img1} className=" w-52 h-52" />
         Hackathon Event
       </div>
     </div>
