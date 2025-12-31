1c239eb89c810ebd5a9b62fc06f0f00c7b1a47eb
mohanavamsi0614 <mohanavamsi14@gmail.com>
2025-12-31 15:28:44 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   const nav = useNavigate();
   return (
     <div className="bg-[#212121] font-poppins flex w-full gap-10  h-screen justify-center items-center">
-      <BackgroundBeams className="fixed inset-0 z-0" />
+      {/* <BackgroundBeams className="fixed inset-0 z-0" /> */}
       <div
         className="relative z-10 cursor-pointer  border-2  w-80 p-15 flex justify-center items-center hover:bg-gray-300 hover:text-black hover:border-black rounded-xl text-white text-center font-semibold"
         onClick={() => nav("/qr")}
